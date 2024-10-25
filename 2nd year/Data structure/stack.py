b05a94889d62d77ea16fe594aa32655f7bbd3c69
Loai Wael <princeloai70@gmail.com>
2024-10-25 10:04:52 +0300

    ADD stack.py
    New task has shown up, Stay tuned.

---
+++ 2nd year/Data structure/stack.py
@@ -0,0 +1,49 @@
+class Stack:
+
+    def __init__(self,max):
+        while type(max)!=int:
+            print("Invalid size or input type. Please enter the size of the stack correctly.")
+        self.stack=[0]*max
+        self.top=-1
+
+    def __str__(self):
+        return str(self.stack)
+    
+    def __len__(self):
+        return len(self.stack)
+    
+    def add(self,num):
+        if self.top == len(self.stack)-1:
+            return print("The stack is full!")
+        else:
+            self.top+=1
+            self.stack[self.top]=num
+
+    def pop(self):
+        if self.top == -1:
+            return print("The stack is empty!")
+        else:
+            temp=self.stack[self.top]
+            self.stack[self.top]=0
+            self.top-=1
+            return temp
+    
+
+# testing
+# x=Stack(5)
+# x.add(5)
+# x.add("loai")
+# x.add(10)
+# x.add('Wael')
+# x.add('Hassan')
+# x.add(1)
+# x.pop()
+# print(x)
+# print(x.pop())
+# print(x)
+# x.pop()
+# x.pop()
+# x.pop()
+# print(x)
+# x.pop()
+# print(len(x))
