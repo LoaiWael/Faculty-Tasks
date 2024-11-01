6f4f2734dd48d59236de6b175043e3360730cd91
Loai Wael <princeloai70@gmail.com>
2024-11-01 20:36:54 +0200

    ADD Map.py , UPD issues in other files

--- 2nd year/Data structure/BinarySearch.py
+++ 2nd year/Data structure/BinarySearch.py
@@ -5,17 +5,12 @@ def binarySearch(li,x):
     while low <= high:
         mid = (low + high) // 2
         if li[mid] == x:
-            return f"the middle of number {x} is {mid}"
+            return mid
         elif li[mid] < x:
             low = mid + 1
         else:
             high = mid - 1
             
-    return f"This number {x} is not included in the list."
+    return f"This number {x} is not included."
 
-li=[2,4,6,8,10,12,16]
-#   0,1,2,3, 4, 5,6
-
-x= int(input(f"choose a number in the list: {li}"))
-print(binarySearch(li,x))
         
--- 2nd year/Data structure/stack.py
+++ 2nd year/Data structure/stack.py
@@ -2,7 +2,7 @@ class Stack:
 
     def __init__(self,max):
         while type(max)!=int:
-            print("Invalid size or input type. Please enter the size of the stack correctly.")
+            return "Invalid size or input type. Please enter the size of the stack correctly."
         self.__stack=[0]*max
         self.__top=-1
 
@@ -20,37 +20,17 @@ class Stack:
     
     def push(self,num):
         if self.__top == len(self.__stack)-1:
-            return print("The stack is full!")
+            return "The stack is full!"
         else:
             self.__top+=1
             self.__stack[self.__top]=num
 
     def pop(self):
         if self.__top == -1:
-            return print("The stack is empty!")
+            return "The stack is empty!"
         else:
             temp=self.__stack[self.__top]
             self.__stack[self.__top]=0
             self.__top-=1
             return temp
     
-
-# testing
-# x=Stack(5)
-# x.push(5)
-# x.push("loai")
-# x.push(10)
-# x.push('Wael')
-# x.push('Hassan')
-# x.push(1)
-# x.pop()
-# print(x)
-# print(x.pop())
-# print(x)
-# x.pop()
-# x.pop()
-# x.pop()
-# print(x)
-# x.pop()
-# print(len(x))
-# print(x.peek())
