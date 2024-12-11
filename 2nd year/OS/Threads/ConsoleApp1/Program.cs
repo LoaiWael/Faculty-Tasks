551e13fb8aa3cb30b41ac78bdd712f0a8eb67d77
Loai Wael <princeloai70@gmail.com>
2024-12-12 01:42:14 +0200

    ADD os course using c#
    stay tunned

---
+++ 2nd year/OS/Threads/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+int total = 0;
+
+Console.WriteLine("current thread: main");
+calc(0, 500);
+Console.WriteLine(total);
+Console.WriteLine("end main thread");
+total=0;
+Thread th1 = new Thread(() => calc(1, 500000));
+Console.WriteLine(total);
+void calc(int start, int end)
+{
+    int temp = 0;
+    for (int i = start; i <= end; i++)
+    {
+        temp += i;
+    }
+    total += temp;
+}
